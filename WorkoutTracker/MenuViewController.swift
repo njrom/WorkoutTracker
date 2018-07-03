7b8434fe75b94ff26be1764157d998139f6277ee
njrom <nromano2@u.rochester.edu>
2018-07-03 12:24:49 -0400

    addTemplate prompts for name of new template then segues to the workout builder view

---
+++ WorkoutTracker/MenuViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  WorkoutTracker
+//
+//  Created by Nicholas Romano on 7/2/18.
+//  Copyright © 2018 Nicholas Romano. All rights reserved.
+//
+
+import UIKit
+
+var templateSessions = [Session]()
+class MenuViewController: UIViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var newTemplateSession = Session(context : self.context)
+        templateSessions.append(newTemplateSession)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func addTemplatePressed(_ sender: Any) {
+        createTemplate()
+    }
+    
+    func createTemplate() {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add a new Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            //What will happen when the user clicks the add item button on alert
+            
+            templateSessions.last?.name = textField.text!
+            self.performSegue(withIdentifier: "goToWorkoutBuilder", sender: self)
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+}
+
---
+++ WorkoutTracker/WorkoutBuilderViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WorkoutBuilderViewController.swift
+//  WorkoutTracker
+//
+//  Created by Nicholas Romano on 7/2/18.
+//  Copyright © 2018 Nicholas Romano. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class WorkoutBuilderViewController: UIViewController {
+    var currentSession = templateSessions.last
+    
+    @IBOutlet weak var sessionNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sessionNameLabel.text = currentSession?.name
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+
+
+}
